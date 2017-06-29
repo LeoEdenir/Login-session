80dd624b9ac0c87856f213f797c33916659a2746
HugoGutzmann <facahugo@gmail.com>
2017-06-29 11:19:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
-//    session_start();
-//    if (isset($_SESSION['esta_logado']) == false OR $_SESSION['esta_logado'] == false);
-//
-//        //usuario nao logado
-//        header('Location: login.php');
+    session_start();
+    
+	$esta_logado = isset($_SESSION['esta_logado']);
+
+        if ($esta_logado == false OR $_SESSION['esta_logado'] == false)
+        //usuario nao logado
+        header('Location: login.php');
 
 
 ?>
@@ -24,7 +26,7 @@
 
 		<a href="verifica_usuario.php?acao=sair" class="sair">sair</a>
 
-		<img src="16938621_608057322723364_3741103415265776794_n.jpg" alt="" width="200" height="200">
+		<img src="https://static.mundodasmensagens.com/upload/listas/m/e/mensagens-de-bom-dia-ND5O3-fxl.png" alt="" width="200" height="200">
 		<h3>Bem vindo!</h3>
 	</div>
 
